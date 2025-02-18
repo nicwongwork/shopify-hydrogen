9881f1ea1b6fde1e8331abc37dd65820d2ccb8fa
Nic Wong <nicwongwork@hotmail.com>
2025-02-18 17:56:42 +0800

    test hello

---
+++ app/routes/_index.jsx
@@ -0,0 +1,16 @@
+export const meta = () => {
+    return [
+        {
+            title: 'Hello World',
+            description: 'DLLM',
+        }
+    ]
+}
+
+export default function Index() {
+    return (
+        <div>
+        <h1>Hello World!</h1>
+        </div>
+    )
+}
