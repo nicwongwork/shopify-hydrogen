558122984bea62e45092e50e9f737aa66088496e
Nic Wong <nicwongwork@hotmail.com>
2025-02-19 12:25:24 +0800

    add preview

---
+++ app/routes/api.edit.ts
@@ -0,0 +1,5 @@
+import { previewModeAction, previewModeLoader } from '@pack/hydrogen'
+import { type ActionFunction, type LoaderFunction } from '@shopify/remix-oxygen'
+
+export const action: ActionFunction = previewModeAction
+export const loader: LoaderFunction = previewModeLoader
